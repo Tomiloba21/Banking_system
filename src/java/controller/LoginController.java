5bfe5aca3bb3fc190452345aaf59bbea4e70fcbd
Tomiloba21 <olowo15tomiloba@gmail.com>
2024-03-20 15:02:00 +0100

    created a service packages

--- src/java/controller/LoginController.java
+++ src/java/controller/LoginController.java
@@ -8,10 +8,12 @@ import ICHDApp.Account;
 import ICHDApp.Customer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import service.LoginService;
 
 /**
  *
---
+++ src/java/service/LoginService.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package service;
+
+/**
+ *
+ * @author admin
+ */
+public class LoginService {
+    public String sayHello(String name){
+        return "Hello "+ name;
+    }
+    
+}
